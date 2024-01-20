332e44f8677fac6947faacdae65b4d711b745f01
steven <sarmiento.steven@gmail.com>
2024-01-19 20:43:33 -0500

    playground push

---
+++ swift-playground/swift_playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_playgroundApp.swift
+//  swift-playground
+//
+//  Created by Steven Sarmiento on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct swift_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

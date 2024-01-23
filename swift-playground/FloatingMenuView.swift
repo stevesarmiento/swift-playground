cf7a8677f70a0075e334b5c4995fe8fca8b2bbd2
steven <sarmiento.steven@gmail.com>
2024-01-22 19:18:03 -0500

    button fun

--- swift-playground/FloatingMenuView.swift
+++ swift-playground/FloatingMenuView.swift
@@ -11,43 +11,46 @@ struct FloatingMenuView: View {
     @Binding var isMenuOpen: Bool
 
     var body: some View {
-        GeometryReader { geometry in 
-         VStack {
+        VStack {
             Spacer()
 
-            if isMenuOpen {
+            ZStack {
                 Button(action: {
                     print("Button 1 tapped")
                 }) {
                     CircleButton(iconName: "1.circle", isMenuOpen: isMenuOpen)
                 }
+                .offset(x: isMenuOpen ? -70 : 0, y: 0)
+                .opacity(isMenuOpen ? 1 : 0)
+                .animation(.spring().speed(0.9), value: isMenuOpen)
 
                 Button(action: {
                     print("Button 2 tapped")
                 }) {
                     CircleButton(iconName: "2.circle", isMenuOpen: isMenuOpen)
                 }
-                .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
-
+                .offset(x: isMenuOpen ? 70 : 0, y: 0)
+                .opacity(isMenuOpen ? 1 : 0)
+                .animation(.spring().speed(0.9), value: isMenuOpen)
 
                 Button(action: {
                     print("Button 3 tapped")
                 }) {
                     CircleButton(iconName: "3.circle", isMenuOpen: isMenuOpen)
                 }
-                .position(x: geometry.size.width / 2, y: geometry.size.height / 2 - 60)
+                .offset(y: isMenuOpen ? -70 : 0)
+                .opacity(isMenuOpen ? 1 : 0)
+                .animation(.spring().speed(0.9), value: isMenuOpen)
 
-            }
-
-            Button(action: {
-                withAnimation {
-                    isMenuOpen.toggle()
+                Button(action: {
+                    withAnimation {
+                        isMenuOpen.toggle()
+                    }
+                }) {
+                    CircleButton(iconName: isMenuOpen ? "xmark" : "plus", isMenuOpen: isMenuOpen)
                 }
-            }) {
-                CircleButton(iconName: isMenuOpen ? "xmark" : "plus", isMenuOpen: isMenuOpen)
             }
-        }
-        .padding()       
+            .padding()
         }
     }
 }

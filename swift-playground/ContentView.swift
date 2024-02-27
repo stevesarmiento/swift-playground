b81ae0e00c5e8550be5268426896dd30c8d1d671
Steven Sarmiento <sarmiento.steven@gmail.com>
2024-02-26 22:43:00 -0500

    fidget bits

--- swift-playground/ContentView.swift
+++ swift-playground/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 struct ContentView: View {
     @State private var position = CGSize.zero
     @State private var rotationAngle = 0.0
+    // @State private var lastHapticAngle = 0.0
     @State private var isSpinning = false
+    @State private var isScaledUp = false
+    @State private var hapticFeedbackTriggered = false
 
 var body: some View {
         GeometryReader { _ in
@@ -26,17 +29,41 @@ var body: some View {
                     .frame(width: 200, height: 200)
                     .offset(position)
                     .rotationEffect(.degrees(rotationAngle))
+                    .scaleEffect(isScaledUp ? 1.2 : 1)
                     .gesture(
-                        DragGesture()
-                            .onChanged { gesture in
-                                self.position = gesture.translation
-                       }
-                            .onEnded { _ in
-                                withAnimation {
-                                    self.position = .zero
+                        TapGesture()
+                            .simultaneously(with: DragGesture(minimumDistance: 0)
+                                .onChanged { gesture in
+                                    withAnimation(.easeIn(duration: 0.1)) {
+                                        self.isScaledUp = true
+                                        self.position = gesture.translation
+                                        if !self.hapticFeedbackTriggered {
+                                            let impactFeedbackGenerator = UIImpactFeedbackGenerator(style: .light)
+                                            impactFeedbackGenerator.impactOccurred()
+                                            self.hapticFeedbackTriggered = true
+                                        }
+                                    }
                                 }
-                            }
+                                .onEnded { _ in
+                                    withAnimation(.easeOut) {
+                                        self.isScaledUp = false
+                                        self.position = .zero
+                                        self.hapticFeedbackTriggered = false // Reset for the next gesture
+                                    }
+                                }
+                            )
                     )
+                    // .gesture(
+                    //     DragGesture()
+                    //         .onChanged { gesture in
+                    //             self.position = gesture.translation
+                    //         }
+                    //         .onEnded { _ in
+                    //             withAnimation {
+                    //                 self.position = .zero
+                    //             }
+                    //         }
+                    // )
                 VStack {
                     Spacer()
                     ArcNavvy()
@@ -48,13 +75,21 @@ var body: some View {
                         let swipeDistance = gesture.translation.width
                         let rotationDegrees = swipeDistance / 2
                         self.rotationAngle = -rotationDegrees
+
+                            // let hapticThreshold = 10.0
+                            // let rotationDifference = abs(self.rotationAngle - self.lastHapticAngle)
+                            // if rotationDifference >= hapticThreshold {
+                            //     let impactFeedbackGenerator = UIImpactFeedbackGenerator(style: .soft)
+                            //     impactFeedbackGenerator.impactOccurred()
+                            //     self.lastHapticAngle = self.rotationAngle
+                            // }
                     }
                     .onEnded { gesture in
                         let swipeVelocity = gesture.predictedEndTranslation.width - gesture.translation.width
                         let additionalRotation = swipeVelocity * 5
                         let finalRotation = self.rotationAngle + -additionalRotation
                         let normalizedRotation = ((finalRotation / 360).rounded()) * 360
-                        withAnimation(Animation.easeOut(duration: 2)) {
+                        withAnimation(Animation.easeOut(duration: 3)) {
                             self.rotationAngle = normalizedRotation
                         }
                     }
